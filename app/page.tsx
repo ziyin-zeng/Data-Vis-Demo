fdd40cf9a59d13e757a43d4d506946a0510831cf
ZiyinFRZ <ziyin.frz@outlook.com>
2024-06-23 18:04:11 +0200

    [Fix] Fix of #11, add md & xl break points

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     // [group] is the trigger for hover animation, can not be re-named
     <div className="group w-3/4 h-[67vh] mx-auto text-center">
-      <div className="relative mt-[33vh] text-9xl/[180px] z-20 animate-upAndDownEaseIn group-hover:animate-upAndDownEaseOut">Glucose Data</div>
-      <div className="relative mt-[-120px] text-9xl/[180px] text-gray-700 z-10 animate-downAndUpEaseOut group-hover:animate-downAndUpEaseIn">Visualization</div>
+      <div className="relative mt-[43vh] md:mt-[38vh] xl:mt-[33vh] text-3xl/[60px] md:text-6xl/[120px] xl:text-9xl/[180px] z-20 animate-upAndDownEaseIn group-hover:animate-upAndDownEaseOut">Glucose Data</div>
+      <div className="relative mt-[-40px] md:mt-[-80px] xl:mt-[-120px] text-3xl/[60px] md:text-6xl/[120px] xl:text-9xl/[180px] text-gray-700 z-10 animate-downAndUpEaseOut group-hover:animate-downAndUpEaseIn">Visualization</div>
       {/* start with opacity 0, since no hover is triggered */}
       {/* when hover is triggered, delay for a while and show the link */}
-      <div className="mt-[30px] w-full flex flex-row justify-center items-center mx-auto text-gray-500 transition opacity-0 ease-in-out delay-500 duration-500 group-hover:opacity-100">
+      <div className="mt-[10px] md:mt-[20px] xl:mt-[30px] w-full flex flex-row justify-center items-center mx-auto text-gray-500 transition opacity-0 ease-in-out delay-500 duration-500 group-hover:opacity-100">
         <Image
           src="/circle-right-regular.svg"
           alt="Redirect icon"
@@ -19,7 +19,7 @@ export default function Home() {
           priority
         />
         <Link href="/login">
-          <div className="ml-2 underline text-xl">Explore</div>
+          <div className="ml-2 underline text-base md:text-lg xl:text-xl">Explore</div>
         </Link>
       </div>
     </div>
