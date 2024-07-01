673262e96490eaef00ae5a17ff1c8e904f7677b2
ZiyinFRZ <ziyin.frz@outlook.com>
2024-07-01 16:11:13 +0200

    [Fix] First render intial data opt.

--- app/ui/detail/GlucoseAnalysis.tsx
+++ app/ui/detail/GlucoseAnalysis.tsx
@@ -25,9 +25,9 @@ export default function GlucoseAnalysis(props: GlucoseAnalysisProps) {
     // without the [0], on first load, the Card component doesn't have anything to show
     const glucoseValue = glucoseData.length ? getAllGlucoseValue(glucoseData) : [0];
 
-    const avgGlucose = glucoseData.length ? averageGlucose(glucoseValue) : '0';
+    const avgGlucose = glucoseData.length ? averageGlucose(glucoseValue) : '---';
 
-    const tir = glucoseData.length ? tirGlucose(glucoseValue) : '0';
+    const tir = glucoseData.length ? tirGlucose(glucoseValue) : '---';
 
     return (
         <React.Fragment>
@@ -41,7 +41,7 @@ export default function GlucoseAnalysis(props: GlucoseAnalysisProps) {
             </div>
             <div className='px-4 flex flex-col items-center lg:justify-between lg:flex-row lg:px-10'>
                 <Box className="w-[80%] lg:min-w-[25%] lg:max-w-[33%]">
-                    <Card variant="outlined"><GlucoseDataCard value={'' + glucoseValue[glucoseValue.length - 1]} text="Latest glucose data" type="latest" /></Card>
+                    <Card variant="outlined"><GlucoseDataCard value={glucoseValue.length === 1 && glucoseValue[0] === 0 ? '---' : '' + glucoseValue[glucoseValue.length - 1]} text="Latest glucose data" type="latest" /></Card>
                 </Box>
                 <Box className="w-[80%] lg:min-w-[25%] lg:max-w-[33%]">
                     <Card variant="outlined"><GlucoseDataCard value={avgGlucose} text="Average glucose in past 24h" type="average" /></Card>
